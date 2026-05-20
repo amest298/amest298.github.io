cee8fea8f9252b89b7d8760b1d220f2cf2e6fc5f
amest298 <amelie.forrester@southlandgirls.school.nz>
2026-05-20 13:11:38 +1200

    adding second page

--- app.js
+++ app.js
@@ -71,3 +71,78 @@ function formatDate(dateString) {
 }
 
 renderPastPeriods();
+
+const newcycleFormEl = document.getElementsByTagName("form")[0];
+const startDateInputEl = document.getElementById("start-date");
+const endDateInputEl = document.getElementById("end-date");
+const pastcycleContainer = document.getElementById("past-cycles");
+
+// Add the storage key as an app-wide constant
+const STORAGE_KEY = "cycle-tracker";
+
+// Listen to form submissions.
+newcycleFormEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const startDate = startDateInputEl.value;
+  const endDate = endDateInputEl.value;
+  if (checkDatesInvalid(startDate, endDate)) {
+    return;
+  }
+  storeNewcycle(startDate, endDate);
+  renderPastcycles();
+  newcycleFormEl.reset();
+});
+
+function checkDatesInvalid(startDate, endDate) {
+  if (!startDate || !endDate || startDate > endDate) {
+    newcycleFormEl.reset();
+    return true;
+	
+  }
+  return false;
+}
+
+function storeNewcycle(startDate, endDate) {
+  const cycles = getAllStoredcycles();
+  cycles.push({ startDate, endDate });
+  cycles.sort((a, b) => new Date(b.startDate) - new Date(a.startDate));
+  window.localStorage.setItem(STORAGE_KEY, JSON.stringify(cycles));
+}
+
+function getAllStoredcycles() {
+  const data = window.localStorage.getItem(STORAGE_KEY);
+  const cycles = data ? JSON.parse(data) : [];
+  console.dir(cycles);
+  console.log(cycles);
+  return cycles;
+}
+
+function renderPastcycles() {
+  const pastcycleHeader = document.createElement("h2");
+  const pastcycleList = document.createElement("ul");
+  const cycles = getAllStoredcycles();
+  if (cycles.length === 0) {
+	  pastcycleHeader.textContent = "No past cycles";
+    return;
+  }
+  pastcycleContainer.textContent = "";
+  pastcycleHeader.textContent = "Past cycles";
+  cycles.forEach((cycle) => {
+    const cycleEl = document.createElement("li");
+    cycleEl.textContent = `From ${formatDate(
+      cycle.startDate,
+    )} to ${formatDate(cycle.endDate)}`;
+    pastcycleList.appendChild(cycleEl);
+  });
+
+  pastcycleContainer.appendChild(pastcycleHeader);
+  pastcycleContainer.appendChild(pastcycleList);
+}
+
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  return date.toLocaleDateString("en-US", { timeZone: "UTC" });
+}
+
+renderPastcycles();
+
